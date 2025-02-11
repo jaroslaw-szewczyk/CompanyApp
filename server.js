a1c4ac98c4aa7db1bf5eab39ed4f3973cc5fcd64
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-02-11 22:48:00 +0000

    Add connection to db

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const mongoUrl = 'mongodb://0.0.0.0:27017';
+const client = new MongoClient(mongoUrl);
 
-app.use('/api', employeesRoutes);
-app.use('/api', departmentsRoutes);
-app.use('/api', productsRoutes);
+const connectDB = async () => {
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-})
+  try{
+    await client.connect()
+    console.log('Successfully connected to the database');
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
-});
+    const db = client.db('companyDB');
+
+    db.collection('employees')
+      .find({ department: 'IT' })
+      .toArray()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+
+    app.use('/api', employeesRoutes);
+    app.use('/api', departmentsRoutes);
+    app.use('/api', productsRoutes);
+
+    app.use((req, res) => {
+      res.status(404).send({ message: 'Not found...' });
+    })
+    
+    app.listen('8000', () => {
+      console.log('Server is running on port: 8000');
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+  
+
+}
+
+connectDB();
+
